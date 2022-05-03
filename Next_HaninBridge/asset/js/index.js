6056e3d5815994b72f5ad8248260a13fa2282484
Yang-Min-Seok <83502596+Yang-Min-Seok@users.noreply.github.com>
2022-05-03 22:03:44 +0900

    New HaninBridge index page

---
+++ Next_HaninBridge/asset/js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    // when user opens page -> fade in #title
+    $(function(){
+        $('#title .wrap hgroup h3').fadeIn(2500);
+        $('#title .wrap hgroup h1').fadeIn(2500);
+    });
+    
+
+})
