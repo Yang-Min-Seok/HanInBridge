5adc665aa446d52bc1ccd44385fab50dba7ef4ec
Yang-Min-Seok <83502596+Yang-Min-Seok@users.noreply.github.com>
2022-05-03 23:02:14 +0900

    about us introduction implement

---
+++ Next_HaninBridge/asset/js/About_us.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+    // when user opens page -> slideDown #introduce .wrap hgroup and p
+    $(function(){
+        $('#introduce .wrap hgroup h3').slideDown(2000);
+        $('#introduce .wrap div p').slideDown(2000);
+    });
+
+})
