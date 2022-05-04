4f0e4a81a9622a35af8e87e2c7f11851e31350e3
Yang-Min-Seok <83502596+Yang-Min-Seok@users.noreply.github.com>
2022-05-04 13:52:22 +0900

    aboutus partners implemontion(swiper js)

--- Next_HaninBridge/asset/js/About_us.js
+++ Next_HaninBridge/asset/js/About_us.js
@@ -6,62 +6,19 @@ $(document).ready(function(){
         $('#introduce .wrap div p').slideDown(2000);
     });
 
-    let i = c = 0, onoff=1;
-    
-    // update 'on' class
-    function pagenum(){
-        $(".c_btn_bot>li").removeClass("on");
-        $(".c_btn_bot>li:nth-child("+(c+1)+")").addClass("on")
-    }
-
-    // when user clicks c_btn_bot>li -> take to propal img
-    $(".c_btn_bot>li").click(function(){
-        i = c - $(this).index();
-        c = $(this).index();
-        w += 1280 * i;
-
-        $(".slide").stop().animate({"margin-left":w}, 500);
-        pagenum();
+    // adopt swiper
+    const swiper = new Swiper('.swiper', {
+      
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        }
     });
-
-    // make moveL
-    let w = 0;
-    function moveL(){
-        c++;
-        if(c==4){
-            c=0;
-        };
-        pagenum();
-        $(".slide").stop().animate({"margin-left":w=-1280*c}, 500);
-    }
     
-    // make moveR
-    function moveR(){
-        c--;
-        if(c==-1){
-            c=3;
-        };
-        pagenum()
-        $(".slide").stop().animate({"margin-left":w=-1280*c},500);
-    }
-    
-    // call moveL every 4 sec
-    let Timer = setInterval(moveL, 4000);
-
-    // implement left, right button
-    const l_btn = $('i.fa-circle-arrow-left'), r_btn = $('i.fa-circle-arrow-right');
-    l_btn.click(function(){
-        moveR();
-    })
-    r_btn.click(function(){
-        moveL();
-    })
-
-    // clear Timer when mouse gets over the left, right button
-    $('i.fa-solid').hover(function(){
-        clearInterval(Timer);
-    },function(){ // when coursor gets out of the button, set Timer again
-        Timer=setInterval(moveL,4000);
-    });
-
 })
