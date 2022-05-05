89d7bbc8cc2cbb0ef28c6eb7f2ca40052fa9bedc
Yang-Min-Seok <83502596+Yang-Min-Seok@users.noreply.github.com>
2022-05-05 14:40:29 +0900

    about us pagenation implement

--- Next_HaninBridge/asset/js/About_us.js
+++ Next_HaninBridge/asset/js/About_us.js
@@ -6,19 +6,20 @@ $(document).ready(function(){
         $('#introduce .wrap div p').slideDown(2000);
     });
 
-    // adopt swiper
     const swiper = new Swiper('.swiper', {
-      
-        // If we need pagination
-        pagination: {
-          el: '.swiper-pagination',
-        },
-      
-        // Navigation arrows
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        }
+      // Optional parameters
+      loop: true,
+    
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
     });
     
 })
