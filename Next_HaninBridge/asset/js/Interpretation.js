c0102ee7ad971353e0e7e500768242027a8e836f
Yang-Min-Seok <83502596+Yang-Min-Seok@users.noreply.github.com>
2022-05-06 13:35:53 +0900

    interpretation introduce implemention

---
+++ Next_HaninBridge/asset/js/Interpretation.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+    $(function(){
+        $('#introduce .wrap hgroup h3').css({'transform':'translateX(0)','transition':'all 2s'});
+        $('#introduce .wrap hgroup h2').css({'transform':'translateX(0)','transition':'all 2s'});
+    })
+
+})
