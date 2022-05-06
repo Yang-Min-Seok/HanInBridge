b2270ed874b4a762d6dcaab16f8eed31fb6a63da
Yang-Min-Seok <83502596+Yang-Min-Seok@users.noreply.github.com>
2022-05-06 15:17:16 +0900

    Interpretation interpreter implement

--- Next_HaninBridge/asset/js/Interpretation.js
+++ Next_HaninBridge/asset/js/Interpretation.js
@@ -1,8 +1,21 @@
 $(document).ready(function(){
 
+    // when user opens page -> give animation effect to title
     $(function(){
         $('#introduce .wrap hgroup h3').css({'transform':'translateX(0)','transition':'all 2s'});
         $('#introduce .wrap hgroup h2').css({'transform':'translateX(0)','transition':'all 2s'});
-    })
+    });
+
+    // when user goes to interpreter section -> give animation effect to title
+    $(function() {
+        $(window).scroll(function() {
+            let a = $(this).scrollTop();
+            // for test
+            // console.log(a);
+            if(a > 800){
+                $('#interpreter .wrap hgroup').animate({opacity:'1',transition:'0.7s'});
+            }
+        });
+    });
 
 })
