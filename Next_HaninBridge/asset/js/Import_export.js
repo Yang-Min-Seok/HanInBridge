afb95617e32a8d8aec3d15d87d2637a31406096a
Yang-Min-Seok <83502596+Yang-Min-Seok@users.noreply.github.com>
2022-05-10 13:12:04 +0900

    import_export contact implemention

--- Next_HaninBridge/asset/js/Import_export.js
+++ Next_HaninBridge/asset/js/Import_export.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
     // when user opens page, move hgroup
     $(function(){
-        $('#introduce .wrap hgroup h3').css({transform:'translateX(-10%)',transition:'all 2s'});
-        $('#introduce .wrap hgroup h4').css({transform:'translateX(10%)',transition:'all 2s'});
+        $('#introduce .wrap hgroup h3').css({transform:'translateX(-20%)',transition:'all 2s'});
+        $('#introduce .wrap hgroup h4').css({transform:'translateX(20%)',transition:'all 2s'});
     })
 })
