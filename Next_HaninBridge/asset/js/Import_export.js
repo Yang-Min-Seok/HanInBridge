e68b1c82652f9477c2846a0524d1c00ab21c7a7a
Yang-Min-Seok <83502596+Yang-Min-Seok@users.noreply.github.com>
2022-05-10 12:55:58 +0900

    import export introduce implemention

---
+++ Next_HaninBridge/asset/js/Import_export.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+    // when user opens page, move hgroup
+    $(function(){
+        $('#introduce .wrap hgroup h3').css({transform:'translateX(-10%)',transition:'all 2s'});
+        $('#introduce .wrap hgroup h4').css({transform:'translateX(10%)',transition:'all 2s'});
+    })
+})
