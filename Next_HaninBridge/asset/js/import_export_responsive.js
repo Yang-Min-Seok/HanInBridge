22fb25ae04ce86a8c797928de986dbbf1ba30d17
Yang-Min-Seok <83502596+Yang-Min-Seok@users.noreply.github.com>
2022-05-17 13:42:24 +0900

    Update import_export_responsive.js

--- Next_HaninBridge/asset/js/import_export_responsive.js
+++ Next_HaninBridge/asset/js/import_export_responsive.js
@@ -7,8 +7,8 @@ $(document).ready(function(){
         
         // when user opens page, move hgroup
         $(function(){
-            $('#introduce .wrap hgroup h3').css({'transform':'translateX(-15%)','transition':'all 2s'});
-            $('#introduce .wrap hgroup h4').css({'transform':'translateX(15%)','transition':'all 2s'});
+            $('#introduce .wrap hgroup h3').css({'transform':'translateX(-10%)','transition':'all 2s'});
+            $('#introduce .wrap hgroup h4').css({'transform':'translateX(10%)','transition':'all 2s'});
         })
 
     }
