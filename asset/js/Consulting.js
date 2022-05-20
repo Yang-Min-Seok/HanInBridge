8e181a9ca8134a8c7b4917d0b6edf28dca5f9831
Yang-Min-Seok <83502596+Yang-Min-Seok@users.noreply.github.com>
2022-05-20 14:16:53 +0900

    Consulting page update

--- asset/js/Consulting.js
+++ asset/js/Consulting.js
@@ -12,4 +12,32 @@ $(document).ready(function(){
         
     }
     
+    const swiper = new Swiper('.swiper', {
+      
+        // over effect
+        effect: 'fade',
+        fadeEffect: {
+          crossFade: true
+        },
+        
+        // set auto play
+        autoplay: {
+          delay: 2500,
+        },
+  
+        // Optional parameters
+        loop: true,
+      
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
+      
 })
