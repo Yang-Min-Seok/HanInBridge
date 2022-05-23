44f34da4d292aff4d3b3750fe8e0afe248555825
Yang-Min-Seok <83502596+Yang-Min-Seok@users.noreply.github.com>
2022-05-23 18:08:40 +0900

    interpretation res js update

---
+++ asset/js/Interpretation_responsive.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    
+    let w = $(window).width();
+    
+    // for mobile
+    if(w <= 768){
+        
+        // whe user goes to interpreter section -> make hgroup clear
+        $(function() {
+            $(window).scroll(function() {
+                let a = $(this).scrollTop();
+                // for test
+                console.log(a);
+                if(a > 45){
+                    $('#interpreter .wrap hgroup').animate({opacity:'1',transition:'0.7s'});
+                }
+            });
+        });
+        
+    }
+
+})
