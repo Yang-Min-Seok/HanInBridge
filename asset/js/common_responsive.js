03d54b595690186f3ae2faefbf9f9521c6ff1a1b
Yang-Min-Seok <83502596+Yang-Min-Seok@users.noreply.github.com>
2022-05-29 23:35:01 +0900

    menu btn sliding speed update 1000 -> 700

--- asset/js/common_responsive.js
+++ asset/js/common_responsive.js
@@ -12,15 +12,15 @@ $(document).ready(function(){
 
         // when user clicks #menu_anchor -> slideUp #menu_anchor and slideDown #menu_btn
         $('#menu_anchor').click(function(){
-            $(this).slideUp(1000);
-            $('#menu_btn').slideDown(1000);
+            $(this).slideUp(700);
+            $('#menu_btn').slideDown(700);
             return false;
         })
 
         // when user clicks #menu_btn li:nth-child(1) -> slideUp #menu_btn and slideDown #menu_anchor
         $('#menu_btn li:nth-child(1)').click(function(){
-            $('#menu_btn').slideUp(1000);
-            $('#menu_anchor').slideDown(1000);
+            $('#menu_btn').slideUp(700);
+            $('#menu_anchor').slideDown(700);
             return false;
         })
     }
